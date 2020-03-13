b98d1e42340a4659f8c9369f5b99f822ff29c0db
Fluent85 <tshabalala.richard@gmail.com>
2020-03-13 18:05:37 -0500

    app currently works, fixed javascript issue, added readme info, work info still doesn't populate

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# developer-generator
+# Developer-generator
+
+The goal of this project was to create a command-line application that dynamically generates a PDF profile from a GitHub username. The application will be invoked with the following command:
+
+```sh
+node index.js
+```
+
+The user will be prompted for a favorite color, which will be used as the background color for cards.
+
+The PDF will be populated with the following:
+
+* Profile image
+* user name
+* Links to the following:
+  * User location via Google Maps
+  * User GitHub profile
+  * User blog
+* User bio
+* Number of public repositories
+* Number of followers
+* Number of GitHub stars
+* Number of users following
+
+
+## User Story
+
+AS A product manager
+
+I WANT a developer profile generator
+
+SO THAT I can easily prepare reports for stakeholders
+
+## Business Context
+
+When preparing a report for stakeholders, it is important to have up-to-date information about members of the development team. Rather than navigating to each team member's GitHub profile, a command-line application will allow for quick and easy generation of profiles in PDF format.
+
+## Screenshot
+
+
+<img src="assets/images/github.JPG" alt="github profile image">
+
+
+## Motivation
+
+Get better at writing code in Javascript and Node.js.   
+
+## Issues and future development
+
+Currently the application works, but there are some issues with pulling in work info.  I probably will work on design more, at some CSS.  Also figure out the bug with adding working info.
+
+## Technologies Implemented
+
+HTML 5, CSS, Bootstrap, Google Fonts, jQuery, JavaScript   
+
+
+
+## Credits
+
+--Richard Tshabalala
--- index.js
+++ index.js
@@ -1,79 +1,117 @@
-var inquirer = require("inquirer");
-const axios = require("axios");
+const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require("axios");
 const util = require("util");
 const pdf = require('html-pdf');
-
-
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "gitHubUsername"
-    },
-    {
-        type: "list",
-        message: "Choose your favorite color:",
-        name: "list",
-        choices: ["blue", "red", "green", "orange", "black", "purple"]
-    }
-]).then(function(response){
-    console.log(response)
-    const queryUrl = `https://api.github.com/users/${response.gitHubUsername}/repos?per_page=100`;
-    axios.get(queryUrl).then(function(res){
-        const repos = res.data;
-      
-        const html = generateHTML(repos);
-        next(html);
-        
-    })
-})
-
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function generateHTML(answers) {
-  console.log(answers)
-  return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.html_url}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-}
+function promptUser() { 
+  return inquirer.prompt([
+        {
+          type: "input",
+          message: "What is your name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is your GitHub username?:",
+          name: "github",
+        },
+        {
+          type: "input",
+          message: "What is your favorite color?:",
+          name: "color"
+        },
+  ]);
 
-async function next(html) {
+}
+  function generateHTML(answers, response) {
+    return`
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif&display=swap" rel="stylesheet">
+    <title>GitHub Profile Generator</title>
+  </head>
+  <body style="background-color:black; font-family: 'PT Serif', serif;">
+    <style>
+        a:link {
+            text-decoration: none;
+        }
+        a {
+            color: black;
+        }
+    </style>
+<div class="container" style="width: 960px;">
+        <header style="text-align: center; font-size: 40px;">
+                GitHub Snapshot for ${answers.name}
+            </header>
+<br />
+    <div class="jumbotron jumbotron-fluid;" style="margin: 0 auto; background-color: ${answers.color};">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4"><img class="center-block" src="${response.data.avatar_url};" style="height:300px; width: 300px;"></div>
+            <div class="col-4"></div>
+        </div>
+<br />
+    <h3 style="text-align:center;"><strong>Currently working for:</strong> ${response.data.company}</h3>
+<br />
+        <div class="row">
+            <div class="col-4" style="text-align: center; font-size: 22px;"><i class="fas fa-location-arrow"></i><a href="https://google.com/maps/search/${response.data.location}"> ${response.data.location}</a></div>
+            <div class="col-4" style="text-align: center; font-size: 22px;"><i class="fab fa-github"></i><a href="https://github.com/${answers.github}"> GitHub</a></div>
+            <div class="col-4" style="text-align: center; font-size: 22px;"><i class="fas fa-blog"></i><a href="${response.data.blog}"> Blog</a></div>
+        </div>
+    </div>
+<br />
+    <h2 style="text-align:center;">${response.data.bio}</h2>       
+<br />
+        <div class="row">
+            <div class="col-4 card" style="background-color: ${answers.color}; font-size: 24px; text-align: center; padding: 10px;">Public Repositories: ${response.data.public_repos}</div>
+            <div class="col-4"></div>
+            <div class="col-4 card" style="background-color: ${answers.color}; font-size: 24px; text-align: center; padding: 10px;">GitHub Stars: ${response.data.public_gists}</div>
+        </div>
+<br />
+        <div class="row">
+            <div class="col-4 card" style="background-color: ${answers.color}; font-size: 24px; text-align: center; padding: 10px;">Followers: ${response.data.followers}</div>
+            <div class="col-4"></div>
+            <div class="col-4 card" style="background-color: ${answers.color}; font-size: 24px; text-align: center; padding: 10px;">Following: ${response.data.following}</div>
+        </div>
+<br />
+    </div>
+    </div>
+  </body>
+  </html>`
+      }
+
+  async function init() {
  
-  try {
-    await writeFileAsync("index.html", html);
-
-    var readHtml = fs.readFileSync('index.html', 'utf8');
-    var options = { format: 'Letter' };
-     
-    pdf.create(readHtml, options).toFile('test.pdf', function(err, res) {
-      if (err) return console.log(err);
-      console.log(res); 
-    });
-
-    console.log("Successfully wrote to index.html");
-  } catch (err) {
-    console.log(err);
+    try {
+      const answers = await promptUser();
+      const response = await axios.get("https://api.github.com/users/" + answers.github);
+ 
+      const html = generateHTML(answers, response);
+  
+      await writeFileAsync("index.html", html);
+  
+      var readHtml = fs.readFileSync('index.html', 'utf8');
+      var options = { format: 'Letter',
+                        height:"970px",
+                      width: "970px"};
+  
+      pdf.create(readHtml, options).toFile('profile.pdf', function (err, res) {
+        if (err) return console.log(err);
+        console.log(res);
+      });
+  
+      console.log("Successfully wrote to index.html");
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
+  
+  init();
 
